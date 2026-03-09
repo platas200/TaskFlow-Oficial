c2245203649656daa86e82cbaf87f6438413f5b2
Marcela Platas Revuelta <marceplatas25@gmail.com>
2026-03-08 19:59:53 -0600

    MVP visual: Lista de tareas y formulario

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { 
+  IonHeader, IonToolbar, IonTitle, IonContent, 
+  IonList, IonListHeader, IonItem, IonLabel, 
+  IonBadge, IonNote, IonFab, IonFabButton, 
+  IonIcon, IonItemSliding, IonItemOption, IonItemOptions 
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  standalone: true,
+  imports: [
+    IonHeader, IonToolbar, IonTitle, IonContent, 
+    IonList, IonListHeader, IonItem, IonLabel, 
+    IonBadge, IonNote, IonFab, IonFabButton, 
+    IonIcon, IonItemSliding, IonItemOption, IonItemOptions
+  ],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor() {
+    // Esto registra el icono de "+" para que aparezca en el botón
+    addIcons({ add });
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { 
+  IonHeader, IonToolbar, IonTitle, IonContent, 
+  IonList, IonListHeader, IonItem, IonLabel, 
+  IonBadge, IonNote, IonFab, IonFabButton, 
+  IonIcon, IonItemSliding, IonItemOption, IonItemOptions 
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, 
+    IonList, IonListHeader, IonItem, IonLabel, 
+    IonBadge, IonNote, IonFab, IonFabButton, 
+    IonIcon, IonItemSliding, IonItemOption, IonItemOptions]
 })
 export class Tab2Page {
 
